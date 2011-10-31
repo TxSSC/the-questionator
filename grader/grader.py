6ba23253eb43da46e6b8cc70283cf4fbfaff35a4
Matt McFarland <Vanetix@TSSC-172479.canyon.txstate.edu>
2011-10-31 09:50:00 -0500

    Initial commit

---
+++ grader/grader.py
@@ -0,0 +1,22 @@
+import json
+
+#relative to the runtime environment
+ANSWER_FILE = 'grader/answers.json'
+
+class Grader():
+    #read in the json file
+    answers = open(ANSWER_FILE, 'r').read()
+    answers = json.loads(answers)
+    
+    #check for a correct answer
+    def gradeQuestion(self, question, answer):
+        try:
+            if self.answers[question] == answer:
+                #question is correct
+                return 1
+            else:
+                #question is wrong
+                return 0
+        except KeyError:
+            #question not found
+            return -1
---
+++ settings.py
@@ -0,0 +1,7 @@
+#App config
+
+DEBUG = True
+SECRET_KEY = 'testing'
+MONGODB_DATABASE = 'sexting'
+MONGODB_HOST = 'localhost'
+MONGODB_PORT = 27017
