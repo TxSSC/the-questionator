56123ae887d56b0d7a303b42187057e70e5052b0
Matt McFarland <matt.mcfarland@txstate.edu>
2011-12-14 17:30:50 -0600

    added bootstrap file and requirements

---
+++ bootstrap.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# bootstrap.py
+# Bootstrap and setup a virtualenv with the specified requirements.txt
+import os
+import sys
+import shutil
+import subprocess
+from optparse import OptionParser
+
+
+usage = 'usage: %prog [options]'
+parser = OptionParser(usage=usage)
+parser.add_option('-c', '--clear', dest='clear', action='store_true',
+                help='clear out existing virtualenv')
+parser.add_option('-i', '--init', dest='init', action='store_true',
+                help='Initialize virtual env')
+parser.add_option('-u', '--upgrade', dest='upgrade', action='store_true',
+                help='Upgrade')
+
+def main():
+    if 'VIRTUAL_ENV' not in os.environ or not len(sys.argv) > 1:
+        sys.stderr.write('$VIRTUAL_ENV not found.\n\n')
+        parser.print_usage()
+        sys.exit(-1)
+    
+    (options, pos_args) = parser.parse_args()
+    virtualenv = os.environ['VIRTUAL_ENV']
+
+    if options.init:
+        subprocess.call(['virtualenv', virtualenv])
+    
+    if options.clear:
+        subprocess.call(['virtualenv', '--clear', '--distribute', virtualenv])
+    
+    pip_args = ['pip-python', 'install', '-E', virtualenv, '--requirement', 'questionator/config/requirements.txt']
+
+    if options.upgrade:
+        pip_args.append('--upgrade')
+        subprocess.call(pip_args)
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
