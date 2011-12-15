bd8e6050c8686a5d23d6f38f43e294ad6f91a5ac
Matt McFarland <matt.mcfarland@txstate.edu>
2011-12-15 12:27:03 -0600

    added more functionality to fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,39 @@
 from fabric.api import run, cd, settings
 
 
+APP_DIR = '/home/Vanetix/Documents/projects/testing/'
+VIRTUAL_ENV = 'venv'
+
+
 def deploy():
-    app_dir = 'home/Vanetix/Documents/projects/testing/'
     with settings(warn_only=True):
-        if run("test -d %s" % app_dir).failed:
-            run("git clone git@github.com:/TxSSC/the-questionator.git %s" % app_dir)
-    with cd(app_dir):
-        run("git pull")
-        run("python bootstrap.py -i")
+        if run('test -d %s' % APP_DIR).failed:
+            run('git clone git@github.com:/TxSSC/the-questionator.git %s' % APP_DIR)
+            run('mkdir run')
+            run('export VIRTUAL_ENV="%s"' % VIRTUAL_ENV)
+            run('python bootstrap.py --init')
+            run('python bootstrap.py --upgrade')
+    with cd(APP_DIR):
+        run('git pull')
+        run('python bootstrap.py --upgrade')
+
+
+def clean_env():
+    with cd(APP_DIR):
+        run('python bootstrap.py --clean')
+        run('find . -name "*.pyc" -delete')
+
+
+def reload():
+    with cd(APP_DIR):
+        run('kill -HUP `find . -name "questionator.pid" -exec cat {} \;`')
+
+
+def stop():
+    with cd(APP_DIR):
+        run('kill -INT `find . -name "questionator.pid" -exec cat {} \;`')
+
+
+def start():
+    with cd(APP_DIR):
+        run('%s/bin/gunicorn -c questionator/config/gunicorn.conf engine:app &' % VIRTUAL_ENV)
