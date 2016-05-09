a720aedd24be7ebf65936b181f520b530c5db7bc
Matt McFarland <matmcfarland@gmail.com>
2016-05-09 11:47:49 -0500

    bind to host ip rather than loopback

--- engine.py
+++ engine.py
@@ -4,4 +4,4 @@ from questionator import app
 
 if __name__ == '__main__':
     port = os.environ.get('PORT', 3000)
-    app.run(host='127.0.0.1', port=int(port))
+    app.run(host='0.0.0.0', port=int(port))
